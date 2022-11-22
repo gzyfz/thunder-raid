f818800ac5ec0d9a4714d05c25917448c376f0c0
xiaoyezi <f1ye@ucsd.edu>
2022-11-21 18:45:05 -0800

    randomly-generate-enemy

--- src/Control.hs
+++ src/Control.hs
@@ -4,17 +4,30 @@ module Control where
 import Brick hiding (Result)
 import qualified Graphics.Vty as V
 import qualified Brick.Types as T
-
+import System.Random
+import System.CPUTime
 import Model
 import Board
 
 -------------------------------------------------------------------------------
+hardLevel :: Int
+hardLevel = 80
 
 control :: PlayState -> BrickEvent n Tick -> EventM n (Next PlayState)
 control s ev = case ev of
-  AppEvent Tick                   -> Brick.continue (updateAll s)
-  T.VtyEvent (V.EvKey V.KUp   _)  -> Brick.continue (generate Bullet s)
-  T.VtyEvent (V.EvKey V.KDown _)  -> Brick.continue (generate Enemy s)
+  AppEvent Tick                   ->
+    do
+      ri <- getStdRandom $ randomR (0, 150 :: Int)
+      if ri < hardLevel then Brick.continue (updateAllAndAddEnemy s ri)
+      else Brick.continue (updateAllOnly s)
+  T.VtyEvent (V.EvKey V.KUp   _)  -> 
+    Brick.continue (generate Bullet x s)
+    where x = pCol (playerPos s)
+  T.VtyEvent (V.EvKey V.KDown _)  -> 
+    do
+      ri <- getStdRandom $ randomR (0, 15 :: Int)
+      Brick.continue (generate Enemy ri s)
+  -- ri denotes random integer
   T.VtyEvent (V.EvKey V.KLeft _)  -> Brick.continue (move left  s)
   T.VtyEvent (V.EvKey V.KRight _) -> Brick.continue (move right s)
   T.VtyEvent (V.EvKey V.KEsc _)   -> Brick.halt s
@@ -29,14 +42,25 @@ move f s = s { psBoard = update oldpos f (psBoard s),
                   newpos = f oldpos
 
 -------------------------------------------------------------------------------
-generate :: Piece -> PlayState -> PlayState
+generate :: Piece -> Int -> PlayState -> PlayState
 -------------------------------------------------------------------------------
-generate piece s 
+
+generate piece x s
   | piece == Bullet = s { psBoard = put (Just Bullet) (Pos 2 x)  (psBoard s) }
   | piece == Enemy  = s { psBoard = put (Just Enemy)  (Pos 10 x) (psBoard s) }
-  where x = pCol (playerPos s)
+
+
+
 
 -------------------------------------------------------------------------------
-updateAll :: PlayState -> PlayState
+-- updateAll :: PlayState -> PlayState
 -------------------------------------------------------------------------------
-updateAll s = s { psBoard = refreshAll (psBoard s)}
+updateAllAndAddEnemy s x  =  s { psBoard = refreshAll (psBoard (generate Enemy x s))}
+
+
+updateAllOnly s   =  s { psBoard = refreshAll (psBoard s)}
+
+-- randomly choose whether to refresh the screen or generate a new enemy brfore refreshing
+
+
+--  s { psBoard = refreshAll (psBoard s)}
